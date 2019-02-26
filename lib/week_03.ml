0f22a244754a554ba83882dae642f4a37f6edeb0
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-02-26 11:38:27 +0800

    added copyrights

--- lib/week_03.ml
+++ lib/week_03.ml
@@ -1,10 +1,33 @@
+(*
+This file is part of teaching material of Yale-NUS College module
+"YSC2229: Introductory Data Structures and Algorithms"
+
+Copyright (c) 2019 Ilya Sergey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*)
+
+
 (***********************************)
 (* Searching and sorting in arrays *)
 (***********************************)
 
-(* Load *)
-(* Next line is only necessary for REPL *)
-(* #load "week_02.cmo";;  *)
 open Week_02
 
 (* Generating elements for an array *)
--- lib/week_04.ml
+++ lib/week_04.ml
@@ -1,18 +1,27 @@
-(* Loading previous modules *)
+(*
+This file is part of teaching material of Yale-NUS College module
+"YSC2229: Introductory Data Structures and Algorithms"
 
-(* 
+Copyright (c) 2019 Ilya Sergey
 
-To compile, run
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-ocamlc week_02.ml week_03.ml
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-To load to the REPL, run
-
-ocamlmktop -o mytoplevel week_02.cmo week_03.cmo
-
-and then invoke ./mytoplevel as a REPL command
-
- *)
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*)
 
 open Week_02
 open Week_03
